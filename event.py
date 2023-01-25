80c53c120871fb212e75768d2d088ea224f33604
pkw <yf.pankaiwen@phzp.com>
2023-01-25 17:02:29 +0800

    手动输入获取正确的番号

--- event.py
+++ event.py
@@ -110,8 +110,25 @@ def command():
         _LOGGER.error('QB登录失败')
 
 
+def get_true_code(input_code: str):
+    code_list = input_code.split('-')
+    code = ''.join(code_list)
+    length = len(code)
+    index = length - 1
+    num = ''
+    all_number = '0123456789'
+    while index > -1:
+        s = code[index]
+        if s not in all_number:
+            break
+        num = s + num
+        index = index - 1
+    prefix = code[0:index + 1]
+    return (prefix + '-' + num).upper()
+
+
 def download_by_code(code: str):
-    code = code.upper()
+    code = get_true_code(code)
     if login_qb():
         chapter = get_chapter(code)
         if not chapter:
